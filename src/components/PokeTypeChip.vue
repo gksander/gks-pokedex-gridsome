c13b3d4a6ea9ea70e9b43b7d92f1ad7d2e895e8c
Grant Sander <gksander93@gmail.com>
2020-11-09 06:54:58 -0600

    Make type chips more accessible

--- src/components/PokeTypeChip.vue
+++ src/components/PokeTypeChip.vue
@@ -52,9 +52,9 @@ export default {
     customClasses() {
       switch (this.type.name) {
         case "Bug":
-          return "text-green-600 border-green-600 hover:bg-green-100";
+          return "text-green-700 border-green-600 hover:bg-green-100";
         case "Fire":
-          return "text-orange-600 border-orange-600 hover:bg-orange-100";
+          return "text-orange-700 border-orange-600 hover:bg-orange-100";
         case "Grass":
           return "text-green-700 border-green-700 hover:bg-green-100";
         case "Poison":
@@ -66,17 +66,17 @@ export default {
         case "Normal":
           return "text-gray-900 border-gray-900 hover:bg-gray-100";
         case "Electric":
-          return "text-yellow-600 border-yellow-600 hover:bg-yellow-100";
+          return "text-yellow-800 border-yellow-600 hover:bg-yellow-100";
         case "Ground":
-          return "text-yellow-700 border-yellow-700 hover:bg-yellow-100";
+          return "text-yellow-800 border-yellow-700 hover:bg-yellow-100";
         case "Fairy":
-          return "text-pink-600 border-pink-600 hover:bg-pink-100";
+          return "text-pink-700 border-pink-600 hover:bg-pink-100";
         case "Fighting":
           return "text-red-700 border-red-700 hover:bg-red-100";
         case "Ice":
-          return "text-blue-500 border-blue-500 hover:bg-blue-100";
+          return "text-blue-700 border-blue-500 hover:bg-blue-100";
         case "Ghost":
-          return "text-purple-500 border-purple-500 hover:bg-purple-100";
+          return "text-purple-700 border-purple-500 hover:bg-purple-100";
         case "Psychic":
           return "text-purple-600 border-purple-600 hover:bg-purple-100";
         case "Rock":
@@ -84,7 +84,7 @@ export default {
         case "Dragon":
           return "text-orange-700 border-orange-700 hover:bg-orange-100";
         case "Steel":
-          return "text-gray-600 border-gray-600 hover:bg-gray-100";
+          return "text-gray-700 border-gray-600 hover:bg-gray-100";
         default:
           return "text-gray-900 border-gray-900 hover:bg-gray-100";
       }
